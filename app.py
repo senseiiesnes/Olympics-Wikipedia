9eed35ab803e1405ee35a12c8bab451b16f66dff
Kushal Jha <info.alonelynerd@gmail.com>
2023-09-06 15:46:35 +0530

    Added a preprocessor file to filter useless cols in dataset and rendered the dataset initially.

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import streamlit as st
 import pandas as pd
+import preprocessor
+
+df = pd.read_csv('athlete_events.csv')
+region_df = pd.read_csv('noc_regions.csv')
+
+df = preprocessor.preprocess(df,region_df)
 
 st.sidebar.radio(
     'Select an Option',
     ('Medal Tally','Overall Analysis','Country-wise Analysis','Athlete wise Analysis')
 )
+
+st.dataframe(df)
